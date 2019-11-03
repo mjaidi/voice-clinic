310fa058794acb1d1620e519c5e381b7df54d39e
Majid Jaidi <majid.jaidi@gmail.com>
2019-11-03 21:53:40 +0100

    changed config for gatsby ntelify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,12 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-netlify-cms`,
     {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,

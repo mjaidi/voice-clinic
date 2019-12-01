d27bccdd5c06731fdb2011cb6684391fb1b70cfd
Majid Jaidi <majid.jaidi@gmail.com>
2019-12-01 15:13:55 +0100

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,12 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-153657442-1",
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `First Sign Site Web`,

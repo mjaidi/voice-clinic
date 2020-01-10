a383b09fc8d0354a273c8dc4d428eac571688ce5
Majid Jaidi <majid.jaidi@gmail.com>
2020-01-10 12:09:08 +0100

    resumed cms config

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-netlify-cms",
       options: {
+        manualInit: true,
         enableIdentityWidget: true,
         modulePath: `${__dirname}/src/cms/cms.js`,
       },

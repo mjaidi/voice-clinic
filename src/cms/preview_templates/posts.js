fac97dab047fea86c56d6df454bf10840d141d35
Majid Jaidi <majid.jaidi@gmail.com>
2020-04-08 16:31:31 +0200

    post preview test typo

--- src/cms/preview_templates/posts.js
+++ src/cms/preview_templates/posts.js
@@ -1,29 +1,32 @@
 import React from "react"
-import PropTypes from "prop-types"
+import PropTypes, { any } from "prop-types"
 
-export const PostPreview = ({ entry, widgetFor }) => {
+export const PostPreview = ({ entry, widgetFor, getAsset }) => {
   return (
     <PostsTemplate
       content={entry.getIn(["data", "content"])}
       category={entry.getIn(["data", "category"])}
       title={entry.getIn(["data", "title"])}
+      featuredImage={getAsset(entry.getIn(["data", "featured_image"]))}
     />
   )
 }
 
-PostsPreview.propTypes = {
+PostPreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
   widgetFor: PropTypes.func,
+  getAsset: PropTypes.any,
 }
 
-const PostsTemplate = ({ content, category, title }) => {
+const PostsTemplate = ({ content, category, title, featuredImage }) => {
   return (
     <section className="section">
       <div className="container content">
         <h1 className="title">{title}</h1>
         <h5>Catégorie: {category}</h5>
+        <img src={featuredImage} alt="featured" />
         {content}
       </div>
     </section>
